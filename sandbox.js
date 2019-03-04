d1a7adb8dbb0038031c324575b02aa9ec706b811
bruno1407 <bruno-3232@hotmail.fr>
2019-03-04 18:05:52 +0100

    first

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,30 @@
 /* eslint-disable no-console, no-process-exit */
 const imdb = require('./src/imdb');
 const DENZEL_IMDB_ID = 'nm0000243';
+var fs=require('fs');
+
 
 async function sandbox (actor) {
   try {
     console.log(`📽️  fetching filmography of ${actor}...`);
-    const movies = await imdb(actor);
+    const movies = await imdb(actor);  
     const awesome = movies.filter(movie => movie.metascore >= 77);
-
     console.log(`🍿 ${movies.length} movies found.`);
     console.log(JSON.stringify(movies, null, 2));
     console.log(`🥇 ${awesome.length} awesome movies found.`);
     console.log(JSON.stringify(awesome, null, 2));
-    process.exit(0);
+    
+    var string=JSON.stringify(movies,null,2);
+    fs.writeFile('movies.json',string,function(err) { 
+        if(err) return console.error(err); 
+        console.log('done'); 
+    })
+    var string2=JSON.stringify(awesome,null,2);
+    fs.writeFile('awesome.json',string2,function(err) { 
+        if(err) return console.error(err); 
+        console.log('done'); 
+    })
+    //process.exit(0);
   } catch (e) {
     console.error(e);
     process.exit(1);
