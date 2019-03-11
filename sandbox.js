f03b3d66df9f6a5778f9beb625e2566e734aa706
bruno1407 <bruno-3232@hotmail.fr>
2019-03-11 14:29:53 +0100

    third

--- sandbox.js
+++ sandbox.js
@@ -31,6 +31,22 @@ async function sandbox (actor) {
       collection.insert(awesome, null, function (error, results) {
         if (error) throw error;
     });
+    app.post("/person", (request, response) => {
+      collection.insert(request.body, (error, result) => {
+          if(error) {
+              return response.status(500).send(error);
+          }
+          response.send(result.result);
+      });
+    });
+    app.get("/people", (request, response) => {
+      collection.find({}).toArray((error, result) => {
+          if(error) {
+              return response.status(500).send(error);
+          }
+          response.send(result);
+      });
+  });
       client.close();
     }); 
  
---
+++ sandbox2.js
@@ -0,0 +1,72 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://bruno:azertyuiop@cluster0-8nvxw.mongodb.net/ListOfMovie?retryWrites=true";
+const DATABASE_NAME = "ListOfMovie";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(3000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("Movie");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+app.get("/movies/search", (request, response) => {
+    req=request.query;
+    var metascore=parseInt(req.metascore)
+    var limit=parseInt(req.limit)
+    console.log(req.limit);
+    collection.find({"metascore":{$gte:metascore}}).limit(limit).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+app.get("/movies/populate", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+app.get("/movies", (request, response) => {
+    collection.find({"metascore":{$gte:70}}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/movies/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+app.post("/movies/:id", (request, response) => {
+    req=request.query;
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+        console.log("done");
+    });
+});
